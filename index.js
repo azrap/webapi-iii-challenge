df8f2ef9689a508294f90f05bd9361f211c6fe11
azrap <azraap@gmail.com>
2019-05-30 10:05:32 -0700

    adding the dynamic port inside index.js for Heroku

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 // code away!
-const server = require('./server.js')
+
+//The Old Code:
+// const server = require('./server.js')
+// server.listen(9090, ()=>{
+//     console.log('listening on port 9090')
+// })
 
 
-server.listen(9090, ()=>{
-    console.log('listening on port 9090')
-})
+//The Heroku Code:
+// it's recommended to load configuration for .env as early as possible
+require('dotenv').config(); // add this line as the first thing to run1
+
+const server = require('./api/server.js');
+
+// we'll read the port from the server environment if it is there
+// heroku will have the PORT environment variable set
+const port = process.env.PORT || 4000;
+
+// we can now use that port, if set up by heroku or read from .env or 5000 as a default if not set
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+});
+
+
